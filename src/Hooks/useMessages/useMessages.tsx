1262cd75400e1d2803ea7ec2d054119fe4dcdd28
cesar zambrano <cesarzambranod@gmail.com>
2024-07-20 21:29:20 -0300

    feat: changes

---
+++ src/Hooks/useMessages/useMessages.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
+
+interface Mensaje {
+  autor: string;
+  texto: string;
+  estado: string;
+  dia: string;
+  hora: string;
+  id: string;
+}
+
+interface MensajeGroup {
+  nombre: string;
+  thumbnail: string;
+  ultima_conexion: string;
+  id: number;
+  nro: string;
+  mensajes: Mensaje[];
+}
+
+const fetchMessages = async (): Promise<MensajeGroup[]> => {
+  const response = await fetch('/messaging.json');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+const useMessages = () => {
+  return useQuery<MensajeGroup[], Error>({
+    queryKey: ['messages'],
+    queryFn: fetchMessages,
+  });
+};
+
+export default useMessages;
